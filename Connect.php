9c5907a8dcb7a3e564afb35787fbf00049d66817
Audey Orcajada <audeyorcajada23@gmail.com>
2024-11-25 23:44:16 +0800

    3rd commit

--- Connect.php
+++ Connect.php
@@ -2,7 +2,7 @@
     $host = 'localhost'; 
     $user = 'root'; 
     $pass = ''; 
-    $dbname = 'leaderboard';
+    $dbname = 'leaderboards';
     $port = '3307';
             
     $conn = mysqli_connect($host, $user, $pass, $dbname, $port);
